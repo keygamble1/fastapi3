2c76e332cda2d01f47c4e327aef85d1025bb8fe6
choi <choi@choi.com>
2024-12-06 11:51:17 +0900

    api2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,13 @@ import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
+
   plugins: [svelte()],
-=======
+
   plugins: [
 
     svelte(),
     svgr()],
     
->>>>>>> 4c47fd13265df3d3025914658f70ac89a0357202
+
 })
